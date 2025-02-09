4e3d8b3b12d2a88d49170d64ee2afb0670498c0f
Brian Yang <yangbri1@msu.edu>
2025-02-09 04:29:33 +0000

    Use Set (and HashSet) to determine whether given String value is an isogram (no repeated letters) --- Why? Set can NOT contain duplicate values.

--- src/main/java/Isogram.java
+++ src/main/java/Isogram.java
@@ -1,3 +1,6 @@
+// import Set & HashSet containers from Java's Utility package --- (Set DN contain duplicate values)
+import java.util.Set;
+import java.util.HashSet;
 
 public class Isogram {
     /**
@@ -13,6 +16,29 @@ public class Isogram {
      * @return true if str is an isogram, false otherwise.
      */
     public boolean isIsogram(String str){
-        return false;
+        // create an array of String datatype w/ fixed size of 'str'.length()
+        // String[] strAry = new String[str.length()];
+        char[] charAry = str.toCharArray();
+        // initialize an empty Set of Character type ...
+        // Set like many Java containers only accept Complex datatypes (not primitives) hence why datatype is Wrapper Class 'Character' 
+        Set<Character> charSet = new HashSet<Character>();
+        // implement for-each loop to iterate via each 'char' within 'charAry'
+        for(char ch : charAry){
+            // .add() each 'char' into 'charSet' ...
+            // Note: Since 'Set' only accepts Complex datatypes --- implicit autoboxing occurs here (converting 'char' -> 'Character')
+            charSet.add(ch);
+        }
+
+        // if the length of 'charAry' & size() of 'charSet' are equivalent ...
+        // if(charSet.size() == charAry.length){
+        //     // no char were removed when being appended to Set aka no duplicate letters found -> 'str' is indeed an isogram
+        //     return true;
+        // }
+        // // otw ... 'str' is NOT an isogram -- return false
+        // return false;
+
+        // ternary operator equivalent to above if-else block succinct way showing same logic
+        boolean isogramStatus = (charSet.size() == charAry.length) ? true : false;
+        return isogramStatus;
     }
 }
